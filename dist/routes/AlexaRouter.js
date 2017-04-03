c368f186708bc9007ad2ae568c539aca5ea75ff1
will jones <wjones0@users.noreply.github.com>
2017-04-02 21:26:36 -0400

    fme

--- dist/routes/AlexaRouter.js
+++ dist/routes/AlexaRouter.js
@@ -20,7 +20,34 @@ class AlexaRouter {
                 "shouldEndSession": false
             }
         };
-        res.send(alres);
+        res.send({
+            "version": "1.0",
+            "sessionAttributes": {
+                "supportedHoriscopePeriods": {
+                    "daily": true,
+                    "weekly": false,
+                    "monthly": false
+                }
+            },
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"
+                },
+                "card": {
+                    "type": "Simple",
+                    "title": "Horoscope",
+                    "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+                },
+                "reprompt": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Can I help you with anything else?"
+                    }
+                },
+                "shouldEndSession": false
+            }
+        });
     }
     /**
      * Take each handler, and attach to one of the Express.Router's
