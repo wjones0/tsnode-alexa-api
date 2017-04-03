58fe40422bdff13e3ad14e4eeeb952228b033a46
will jones <wjones0@users.noreply.github.com>
2017-04-02 21:33:23 -0400

    update

--- dist/routes/AlexaRouter.js
+++ dist/routes/AlexaRouter.js
@@ -20,34 +20,7 @@ class AlexaRouter {
                 "shouldEndSession": false
             }
         };
-        res.send({
-            "version": "1.0",
-            "sessionAttributes": {
-                "supportedHoriscopePeriods": {
-                    "daily": true,
-                    "weekly": false,
-                    "monthly": false
-                }
-            },
-            "response": {
-                "outputSpeech": {
-                    "type": "PlainText",
-                    "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"
-                },
-                "card": {
-                    "type": "Simple",
-                    "title": "Horoscope",
-                    "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
-                },
-                "reprompt": {
-                    "outputSpeech": {
-                        "type": "PlainText",
-                        "text": "Can I help you with anything else?"
-                    }
-                },
-                "shouldEndSession": false
-            }
-        });
+        res.send(alres);
     }
     /**
      * Take each handler, and attach to one of the Express.Router's
--- src/routes/AlexaRouter.ts
+++ src/routes/AlexaRouter.ts
@@ -24,34 +24,7 @@ export class AlexaRouter {
                     "shouldEndSession": false
                 }
             }
-        res.send({
-            "version": "1.0",
-            "sessionAttributes": {
-                "supportedHoriscopePeriods": {
-                    "daily": true,
-                    "weekly": false,
-                    "monthly": false
-                }
-            },
-            "response": {
-                "outputSpeech": {
-                    "type": "PlainText",
-                    "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"
-                },
-                "card": {
-                    "type": "Simple",
-                    "title": "Horoscope",
-                    "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
-                },
-                "reprompt": {
-                    "outputSpeech": {
-                        "type": "PlainText",
-                        "text": "Can I help you with anything else?"
-                    }
-                },
-                "shouldEndSession": false
-            }
-        });
+        res.send(alres);
     }
 
     /**
